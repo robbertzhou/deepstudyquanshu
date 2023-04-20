580b0a14d881570614055a4c80d7b538740fe111
zhouyu1 <zhouyu1@unicdata.com>
2023-04-20 19:31:52 +0800

    init

--- jinjie/concatenateoperate.py
+++ jinjie/concatenateoperate.py
@@ -1,24 +1,3 @@
-import tensorflow as tf
-import numpy as np
-from sklearn.datasets import load_iris
-data = load_iris()
-iris_data = np.float32(data.data)
-iris_target = data.target
-iris_target = np.float32(tf.keras.utils.to_categorical(iris_target,num_classes=3))
-train_data = tf.data.Dataset.from_tensor_slices((iris_data,iris_target)).batch(128)
-input_xs = tf.keras.Input(shape=(4,),name='input_xs')
-out = tf.keras.layers.Dense(32,activation='relu')(input_xs)
-out = tf.keras.layers.Dense(64,activation='relu')(out)
-logits = tf.keras.layers.Dense(3,activation='softmax')(out)
-model = tf.keras.Model(inputs = input_xs,outputs = logits)
-opt = tf.optimizers.Adam(1e-3)
-model.compile(optimizer=opt,loss = tf.losses.categorical_crossentropy,metrics=['accuracy'])
-model.fit(train_data,epochs=500)
-score = model.evaluate(iris_data,iris_target)
-model.save("iris.h5")
-# 150	4	setosa	versicolor	virginica
-
-
 import matplotlib.pyplot as plt
 from tqdm.notebook import tqdm
 
@@ -32,29 +11,22 @@ from sklearn.metrics import mean_absolute_error as mae
 from sklearn.preprocessing import RobustScaler, normalize
 from sklearn.model_selection import train_test_split, GroupKFold, KFold
 from IPython.display import display
-import pandas as pd
 
 COMPUTE_LSTM_IMPORTANCE = 1
 ONE_FOLD_ONLY = 1
 gpu_strategy = tf.distribute.get_strategy()
-
-EPOCH = 300
-BATCH_SIZE = 1024
-NUM_FOLDS = 10
-
 with gpu_strategy.scope():
     kf = KFold(n_splits=2, shuffle=True, random_state=2021)
     test_preds = []
-    for fold, (train_idx, test_idx) in enumerate(kf.split(iris_data, iris_target)):
+    for fold, (train_idx, test_idx) in enumerate(kf.split(train, targets)):
         K.clear_session()
 
         print('-' * 15, '>', f'Fold {fold + 1}', '<', '-' * 15)
-        X_train, X_valid = iris_data[train_idx], iris_data[test_idx]
-        y_train, y_valid = iris_target[train_idx], iris_target[test_idx]
+        X_train, X_valid = train[train_idx], train[test_idx]
+        y_train, y_valid = targets[train_idx], targets[test_idx]
 
         # 导入已经训练好的模型
-        model = keras.models.load_model('iris.h5')
-        COLS = ["150",	"4",	"setosa",	"versicolor",	"virginica"]
+        model = keras.models.load_model('models/XXX.h5')
         # 计算特征重要性
         if COMPUTE_LSTM_IMPORTANCE:
             results = []
@@ -62,15 +34,15 @@ with gpu_strategy.scope():
 
             for k in tqdm(range(len(COLS))):
                 if k > 0:
-                    save_col = X_valid[:, k - 1].copy()
-                    np.random.shuffle(X_valid[:, k - 1])
+                    save_col = X_valid[:, :, k - 1].copy()
+                    np.random.shuffle(X_valid[:, :, k - 1])
 
                 oof_preds = model.predict(X_valid, verbose=0).squeeze()
                 mae = np.mean(np.abs(oof_preds - y_valid))
                 results.append({'feature': COLS[k], 'mae': mae})
 
                 if k > 0:
-                    X_valid[:, k - 1] = save_col
+                    X_valid[:, :, k - 1] = save_col
 
             # 展示特征重要性
             print()
